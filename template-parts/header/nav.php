e8c798410955e502a0b4a0ec937abec66ca4490f
Hayk <yeritsyan04@gmail.com>
2022-11-04 19:12:32 +0400

    remove bootstrap

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -10,23 +10,21 @@ $header_menu_id = $menu_class->get_menu_id( 'abovestate-header-menu' );
 
 $header_menus = wp_get_nav_menu_items( $header_menu_id );
 
+$navbarCount = 1;
+
 ?>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+<nav class="navbar">
     <?php
       if(function_exists( 'the_custom_logo' ) ) {
         the_custom_logo();
       }
     ?>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="navbar__inner">
       <?php 
         if( ! empty( $header_menus ) && is_array( $header_menus ) ){
           ?>
-          <ul class="navbar-nav me-auto">
+          <ul class="navbar__list">
 
             <?php 
               foreach ($header_menus as $menu_item ) {
@@ -37,25 +35,26 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
 
                   if ( ! $has_children ) {
                     ?>
-                    <li class="nav-item">
-                      <a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+                    <li class="navbar__item">
+                      <a class="navbar__link" href="<?php echo esc_url( $menu_item->url ); ?>">
                         <?php echo esc_html( $menu_item->title ); ?>
                       </a>
                     </li>
                     <?php
                   } else {
                     ?>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <li class="navbar__item dropdown">
+                      <a class="navbar__link dropdown__inner" href="<?php echo esc_url( $menu_item->url ); ?>" id="<?php echo 'navbarDropdown' . $navbarCount ?>">
                         <?php echo esc_html( $menu_item->title ); ?>
                       </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <div class="dropdown__menu" id="<?php echo 'navbarDropdownList' . $navbarCount ?>">
                         <?php 
                           foreach( $child_menu_items as $child_menu_item ) {
                             ?>
-                            <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>"><?php echo esc_html( $child_menu_item->title ); ?></a>
+                            <a class="dropdown__item" href="<?php echo esc_url( $child_menu_item->url ); ?>"><?php echo esc_html( $child_menu_item->title ); ?></a>
                             <?php
                           }
+                          $navbarCount++;
                         ?>
                       </div>
                     </li>
@@ -74,10 +73,9 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
         
 
       ?>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="header__form" role="search">
+        <input class="header__form-inp" type="search" placeholder="Search">
+        <button class="header__form-btn" type="submit">Search</button>
       </form>
     </div>
-  </div>
 </nav>
